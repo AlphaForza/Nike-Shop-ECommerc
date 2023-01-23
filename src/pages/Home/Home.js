14b569eddac4f621d3e4e4d26f94dfe839c670bb
AlphaDev <30984729+AlphaForza@users.noreply.github.com>
2023-01-23 17:35:32 +0100

    home explore

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './home.scss';
 
 function Home(props) {
-	return <div>Home Page</div>;
+	return (
+		<div className='container'>
+			<h1 className='title'>Welcome to Nike Shop</h1>
+			<p className='desc'>To explore our shop take category up👋</p>
+		</div>
+	);
 }
 
 export default Home;
