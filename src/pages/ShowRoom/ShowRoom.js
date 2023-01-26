858ed9ddb83cd28487d77b726b6918aeecd8b82c
AlphaDev <30984729+AlphaForza@users.noreply.github.com>
2023-01-26 18:11:43 +0100

    filter-low-to-higher

--- src/pages/ShowRoom/ShowRoom.js
+++ src/pages/ShowRoom/ShowRoom.js
@@ -1,23 +1,69 @@
+import { useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import ProductCard from '../../component/ProductCard/ProductCard';
+
 import './showroom.scss';
 
 function ShowRoom() {
 	const { products } = useSelector((state) => state.productStore);
 	const { category } = useParams();
+	const [productSort, setProductSort] = useState([]);
+
+	let activeSort = useRef();
+
+	useEffect(() => {
+		setProductSort(products[category]);
+		// disable radio button to false
+		activeSort.current.checked = false;
+	}, [category]);
+
+	function filterByPrice(event) {
+		if (event.target.value === 'low') {
+			let copySort = productSort.slice();
+			let sort = copySort.sort(function (a, b) {
+				return a.price - b.price;
+			});
+			setProductSort(sort);
+		} else if (event.target.value === 'bigger') {
+			let copySort = productSort.slice();
+			let sort = copySort.sort(function (a, b) {
+				return b.price - a.price;
+			});
+			setProductSort(sort);
+		} else {
+			setProductSort(products[category]);
+		}
+	}
 
 	return (
 		<section className='showroom container'>
 			<aside>
 				<h2 className='filter'>Filter Price</h2>
-				<div className='low'>
+				<div className='filter-category'>
 					<p>From lower to bigger</p>
-					<input type='checkbox' name='low' id='' />
+					<input
+						ref={activeSort}
+						onClick={(event) => filterByPrice(event)}
+						type='radio'
+						name='sort'
+						value='low'
+						id=''
+					/>
+					<p>From bigger to lower</p>
+					<input
+						ref={activeSort}
+						onClick={(event) => filterByPrice(event)}
+						type='radio'
+						name='sort'
+						value='bigger'
+						id=''
+					/>
 				</div>
 			</aside>
 			<article>
-				{products[category].map((el, index) => {
+				{productSort.map((el, index) => {
 					return <ProductCard key={index} product={el} />;
 				})}
 			</article>
